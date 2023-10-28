c8fb94e100cffb597d0a65d206fb16c4a68792e8
prajwal <prajwalshah912@gmail.com>
2023-10-29 03:09:56 +0530

    fade animation added to the gero banner and nav text

--- script.js
+++ script.js
@@ -9,4 +9,33 @@ const circle = () =>{
         document.querySelector("#minCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
     })
 }
+
+function pageAnimation() {
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    });
+    tl.to(".boundingEl", {
+        y: 0,
+        duration: 2,
+        stagger:.2,
+        delay:-1,
+        ease: Expo.easeInOut
+    });
+    tl.from("#hero--footer", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        delay:-1,
+        ease: Expo.easeInOut
+    });
+}
+
+
+
 circle();
+pageAnimation();
