1c2f998f2bb67958fd863e42090b2283a7e2194b
prajwal <prajwalshah912@gmail.com>
2023-10-29 00:40:54 +0530

    body hover animation done

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+const circle = () =>{
+    window.addEventListener('mousemove', (dets) => {
+        // console.log(dets.clientX, dets.clientY)
+        document.querySelector("#minCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+    })
+}
+circle();
